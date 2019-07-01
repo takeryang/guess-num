720d07e8cc9f581c24127a90fddfe8c5ec5850a4
takeryang <takeryang@gmail.com>
2019-07-01 23:00:11 +0800

    No2 file

--- guess-num.py
+++ guess-num.py
@@ -2,13 +2,17 @@ print('這是一個猜數字的遊戲程式')
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count = count + 1
 	num = input('請輸入數字: ')
 	num = int(num)
 	if num == r:
 		print('你猜中了!!')
+		print('這是你猜的第', count, '次')
 		break
 	elif num > r:
 		print('比答案大!!')
 	elif num < r:
 		print('比答案小!!')
+	print('這是你猜的第', count, '次')
